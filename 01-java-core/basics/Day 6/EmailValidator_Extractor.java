20511b5ce3cc596dc77bfbf97995e6dff09b8e15
rachitsharma300 <rachitsharma300@gmail.com>
2025-05-03 23:07:06 +0530

    Topic String

---
+++ 01-java-core/basics/Day 6/EmailValidator_Extractor.java
@@ -0,0 +1,27 @@
+/*
+Email Validator and Extractor (String)
+Objective: Practice String methods like contains(), indexOf(), substring(), split(), endsWith().
+Problem: Write a Java program that:
+•	Accepts a comma-separated list of email addresses.
+•	Validates each one:
+•	Must contain @ and .
+•	Must end with .com or .org
+•	Extracts and prints the username and domain separately.
+Example Input:
+john.doe@gmail.com,invalidEmail,test@company.org
+
+Example Output:
+Valid: john.doe@gmail.com | Username: john.doe | Domain: gmail.com
+Invalid: invalidEmail
+Valid: test@company.org | Username: test | Domain: company.org
+ */
+
+
+import java.util.Scanner;
+
+public class EmailValidator_Extractor
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
