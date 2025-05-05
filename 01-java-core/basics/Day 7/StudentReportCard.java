8dd4dd16f0f51193fb966ed48d96b7eea1702945
rachitsharma300 <rachitsharma300@gmail.com>
2025-05-05 14:06:26 +0530

    Opp concept

---
+++ 01-java-core/basics/Day 7/StudentReportCard.java
@@ -0,0 +1,30 @@
+/*
+Student Report Card
+
+🔹 Objective:
+Use class, instance/static variables, and parameterized constructors.
+🔹 Requirements:
+•	Create a class Student with instance variables: name, rollNo, marks.
+•	Use a static variable to store the schoolName.
+•	Use a parameterized constructor to initialize student details.
+•	Use a method displayDetails() to print student details.
+
+🔹 Input:
+Student s1 = new Student("Amit", 101, 85);
+Student s2 = new Student("Priya", 102, 92);
+🔹 Output:
+School: ABC International
+Name: Amit, Roll No: 101, Marks: 85
+
+School: ABC International
+Name: Priya, Roll No: 102, Marks: 92
+ */
+
+import java.util.Scanner;
+
+public class StudentReportCard
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
