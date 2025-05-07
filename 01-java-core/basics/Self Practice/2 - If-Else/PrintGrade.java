61ac9c097ca79a94a79a3fee709c9330d926fc4f
rachitsharma300 <rachitsharma300@gmail.com>
2025-05-07 21:48:24 +0530

    Pattern

--- 01-java-core/basics/Self Practice/2 - If-Else/PrintGrade.java
+++ 01-java-core/basics/Self Practice/2 - If-Else/PrintGrade.java
@@ -8,12 +8,29 @@ public class PrintGrade
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter Total Marks");
+        System.out.println("Enter Marks");
         int marks = scan.nextInt();
 
-        if (marks >= 85)
+        if (marks >= 90)
         {
-
+            System.out.println("Grade: A");
         }
+        else if (marks >= 80)
+        {
+            System.out.println("Grade: B");
+        }
+        else if (marks >= 50)
+        {
+            System.out.println("Grade: C");
+        }
+        else if (marks >= 33)
+        {
+            System.out.println("Grade: D");
+        }
+        else
+        {
+            System.out.println("Fail");
+        }
+        scan.close();
     }
 }
---
+++ 01-java-core/basics/Self Practice/2 - If-Else/VowelConsonant.java
@@ -0,0 +1,14 @@
+// 7. Check if a character is vowel or consonant
+
+
+import java.util.Scanner;
+
+public class VowelConsonant
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Write Sentence ");
+        String vowel = scan.nextLine(); 
+    }
+}
