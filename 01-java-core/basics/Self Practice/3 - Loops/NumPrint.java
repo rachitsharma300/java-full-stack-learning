91ff13b930ad60b3cab8d17abd9bb2de92da9bdb
rachitsharma300 <rachitsharma300@gmail.com>
2025-05-09 14:02:37 +0530

    Loops

---
+++ 01-java-core/basics/Self Practice/3 - Loops/NumPrint.java
@@ -0,0 +1,41 @@
+/*
+Write a code to get an integer N and print the values from N to 1.
+
+Input Description:
+A single line contains an integer N.
+
+Output Description:
+Print the values from N to 1 in a separate line.
+
+Sample Input:
+10
+Sample Output:
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+ */
+
+
+import java.util.Scanner;
+
+public class NumPrint
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        for (int i = N; i >= 1; i--)
+        {
+            System.out.println(i);
+        }
+        scan.close();
+    }
+}
---
+++ 01-java-core/basics/Self Practice/3 - Loops/OddEvenSeparatePrint.java
@@ -0,0 +1,49 @@
+/*
+ Write a code get an integer number as input and print the odd and even digits of the number separately.
+
+     Input Description:
+     A single line containing an integer.
+
+     Output Description:
+     Print the even and odd integers of the integer in a separate line.
+
+     Sample Input:
+     1234
+     Sample Output:
+     2 4
+     1 3
+ */
+
+
+import java.util.Scanner;
+
+public class OddEvenSeparatePrint
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String numStr = scan.nextLine();
+
+        StringBuilder evenDigits = new StringBuilder();
+        StringBuilder oddDigits = new StringBuilder();
+
+        for (int i = 0; i < numStr.length(); i++)
+        {
+            char ch = numStr.charAt(i);
+            int digit = ch - '0'; // Convert character to integer
+
+            if (digit % 2 == 0)
+            {
+                evenDigits.append(digit).append(" ");
+            }
+            else
+            {
+                oddDigits.append(digit).append(" ");
+            }
+        }
+        System.out.println(evenDigits.toString().trim());
+        System.out.println(oddDigits.toString().trim());
+
+        scan.close();
+    }
+}
