405ffcc5aab52013551bceee9b31b1bb8d5ec9e9
rachitsharma300 <rachitsharma300@gmail.com>
2025-05-24 06:49:06 +0530

    Practice on Array Self

---
+++ 01-java-core/Self Practice/5 - Array/SortArrayAsc.java
@@ -0,0 +1,12 @@
+// Sort array in ascending order
+
+import java.util.Scanner;
+
+public class SortArrayAsc
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
