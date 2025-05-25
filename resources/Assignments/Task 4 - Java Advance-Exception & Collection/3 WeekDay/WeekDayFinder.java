1985fbada094f119562bd05d5092ef69c67e4ddc
rachitsharma300 <rachitsharma300@gmail.com>
2025-05-26 02:50:16 +0530

    Task 4, Question WeekDayFinder with Custom Exception e

--- resources/Assignments/Task 4 - Java Advance-Exception & Collection/3 WeekDay/WeekDayFinder.java
+++ resources/Assignments/Task 4 - Java Advance-Exception & Collection/3 WeekDay/WeekDayFinder.java
@@ -1,3 +1,8 @@
+/*
+Q3. Store name of weekdays in an array (starting from "Sunday" at 0 index). Ask day position from user and print day name.
+Handle array index out of bound exception and give proper message if user enters day index outside range (0-6).
+ */
+
 import java.util.Scanner;
 
 public class WeekDayFinder
