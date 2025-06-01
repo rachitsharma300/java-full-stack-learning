6b276acafe13e858b54f9b61aee8c2c44810c82c
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-01 22:55:11 +0530

    Task 5 - 3. Adv Java (Liststrings Arrays.asList .filter, .collect)

--- resources/Assignments/Task 5 - Java Advance Async Functional/3 Gift For Students/GiftForAStudent.java
+++ resources/Assignments/Task 5 - Java Advance Async Functional/3 Gift For Students/GiftForAStudent.java
@@ -10,13 +10,13 @@ import java.util.stream.Collectors;
 public class GiftForAStudent
 {
     public static void main(String[] args) {
-        // list of Student names in the class
+        // Createing a list of 10 Student names using List Interface
         List<String> students = Arrays.asList("Rachit", "Rahul", "Anish", "Saurav", "Abhi",
                                               "Rohan", "Akash", "Kajal", "Komal", "Alok");
 
-        // Filtering students whose names starts with A.
+        // Using Stream API and Lambda to filter  names that starts with A.
         List<String> specialGiftList = students.stream()
-                .filter(name -> name.startsWith("A")) // Only names starting with A
+                .filter(name -> name.startsWith("A")) // Lambda Expression
                 .collect(Collectors.toList());              // Collect to a new List
 
         System.out.println("Students who will receive special gifts: ");
