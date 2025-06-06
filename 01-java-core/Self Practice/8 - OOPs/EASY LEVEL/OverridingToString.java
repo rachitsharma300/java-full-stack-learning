ccb1a832ec3b976749a936f9c657d06bb1d43d6a
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-06 18:53:47 +0530

    ✔ Object class is the parent of all classes in Java. ✔ toString() method returns string representation of an object. ✔ Default: ClassName@HexCode (not readable) ✔ We override toString() to return human-readable data. ✔ System.out.println(object); → internally calls object.toString() ✔ Use @Override annotation for clarity and safety.

--- 01-java-core/Self Practice/8 - OOPs/EASY LEVEL/OverridingToString.java
+++ 01-java-core/Self Practice/8 - OOPs/EASY LEVEL/OverridingToString.java
@@ -15,18 +15,18 @@ class StudentInfo
     }
 
     //Overriding toString() Method
-
+    @Override
     public String toString()
     {
         return "Student Info: [Name: " + name + ", Age: " + age + ", Percentage: " + percentage + "%]";
     }
 }
-
 public class OverridingToString
 {
     public static void main(String[] args)
     {
-
+        StudentInfo si = new StudentInfo("Rachit", 24, 77.8);
+        System.out.println(si);
     }
 }
 
