d3b1bbc53f940f2e35ed700246e17726ccfee8a6
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-12 04:59:04 +0530

    [Cab Booking System] class Cab done...

--- resources/Projects/Cab Booking System/src/main/java/model/Cab.java
+++ resources/Projects/Cab Booking System/src/main/java/model/Cab.java
@@ -1,2 +1,40 @@
-public class Cab {
+/*  ================================================================================================ */
+/*          Author: Rachit Sharma  |  Project: Cab Booking System | Date: 2025-06-12                */
+/*  ================================================================================================*/
+
+public class Cab
+{
+    private String cabId;
+    private String driverName;
+    private String location;
+    private boolean isAvailable;
+
+    public Cab(String cabId, String driverName, String location, boolean isAvailable)
+    {
+        this.cabId = cabId;
+        this.driverName = driverName;
+        this.location = location;
+        this.isAvailable = isAvailable;
+    }
+
+    // Getter
+    public String getCabId() { return cabId; }
+    public  String getDriverName() { return driverName; }
+    public  String getLocation() { return  location; }
+    public  boolean isAvailable() { return  isAvailable; }
+
+    // Setter
+    public void setLocation(String location) { this.location = location; }
+    public void setAvailable(boolean available) { isAvailable = available; }
+
+    @Override
+    public String toString()
+    {
+        return "Cab{" +
+                "cabId='" + cabId + '\'' +
+                ", driverName='" + driverName + '\'' +
+                ", location='" + location + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
 }
