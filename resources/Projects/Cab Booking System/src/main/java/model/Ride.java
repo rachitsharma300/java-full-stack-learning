be8f2150390e90c6dcaddb6165fc14daaa2ce5a0
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-12 05:14:18 +0530

    [Cab Booking System] class Rider done...

--- resources/Projects/Cab Booking System/src/main/java/model/Ride.java
+++ resources/Projects/Cab Booking System/src/main/java/model/Ride.java
@@ -1,2 +1,40 @@
-public class Ride {
+/*  ================================================================================================ */
+/*          Author: Rachit Sharma  |  Project: Cab Booking System | Date: 2025-06-12                */
+/*  ================================================================================================*/
+
+public class Ride
+{
+    private String userId;
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public Ride(String userId, String name, String phoneNumber, String email)
+    {
+        this.userId = userId;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Getter
+    public String getUserId() { return userId; }
+    public String getName() { return name; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public  String getEmail() { return email; }
+
+    // Setter
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber; }
+    public void setEmail(String email) {this.email =email; }
+
+    @Override
+    public String toString()
+    {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
