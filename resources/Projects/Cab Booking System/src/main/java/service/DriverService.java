e63c7f148f4f0339142878d1b8802de747a9efd2
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-12 05:24:05 +0530

    [Cab Booking System] Service -> Using Lambda features Add new drivers List all drivers Find driver by ID

--- resources/Projects/Cab Booking System/src/main/java/service/DriverService.java
+++ resources/Projects/Cab Booking System/src/main/java/service/DriverService.java
@@ -1,2 +1,34 @@
-public class DriverService {
+/*  ================================================================================================ */
+/*          Author: Rachit Sharma  |  Project: Cab Booking System | Date: 2025-06-12                */
+/*  ================================================================================================*/
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class DriverService
+{
+    private List<Driver> drivers = new ArrayList<>();
+
+    // Add a new Driver
+    public void addDriver(Driver driver)
+    {
+        drivers.add(driver);
+        System.out.println("Driver added: " + driver);
+    }
+
+    // List all driver
+    public void listDrivers()
+    {
+        drivers.forEach(System.out::println); // Lambda use here
+    }
+
+    // Find a Driver by ID
+    public Optional<Driver> findDriverById(String driverId)
+    {
+        return drivers.stream()
+                .filter(d -> d.getDriverId().equals(driverId)) // Lambda
+                .findFirst();
+    }
 }
