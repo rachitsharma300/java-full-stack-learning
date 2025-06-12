3c2b90138926c81635af402252ca94e77424d597
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-12 07:19:46 +0530

    [Cab Booking System] add how much Driver spends time in riding.

--- resources/Projects/Cab Booking System/src/main/java/util/TimeUtils.java
+++ resources/Projects/Cab Booking System/src/main/java/util/TimeUtils.java
@@ -1,2 +1,18 @@
-public class TimeUtils {
+/*  ================================================================================================ */
+/*          Author: Rachit Sharma  |  Project: Cab Booking System | Date: 2025-06-12                */
+/*  ================================================================================================ */
+
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeUtils
+{
+    public static String getCurrentTimestamp()
+    {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        return now.format(formatter);
+    }
 }
