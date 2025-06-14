890974742596da56df95925c661591c961ba8fa2
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-14 21:09:52 +0530

    "Add Emp record to represent employee details with fields for ID, name, department, salary, and joining date"

---
+++ 01-java-core/4 Advanced Java/Day 16/Employee Management System/Emp.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+    public record Emp
+            (String id,
+             String nane,
+             String department,
+             double salary,
+             LocalDate joiningDate)
+    {
+    }
+
