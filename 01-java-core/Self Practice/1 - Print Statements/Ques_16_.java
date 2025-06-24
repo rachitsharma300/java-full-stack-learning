067692c9e34dca9b30304df46bfbfe5892a6a607
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-24 10:50:11 +0530

    [Self Practice] Ques_16_ Str.len()

---
+++ 01-java-core/Self Practice/1 - Print Statements/Ques_16_.java
@@ -0,0 +1,13 @@
+// Input a string and print its length.
+
+import java.util.Scanner;
+
+public class Ques_16_ {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a String");
+        String str = scan.nextLine();
+        System.out.println("Length of the string is: " +str.length());
+    }
+}
