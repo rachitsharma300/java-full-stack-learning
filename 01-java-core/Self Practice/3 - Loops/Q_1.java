67ab19fb481d0a7af478564e2389eeffe2465543
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-25 17:57:46 +0530

    [Self Practice] Ques_1 1 to 10

---
+++ 01-java-core/Self Practice/3 - Loops/Q_1.java
@@ -0,0 +1,18 @@
+// Print 1 to 10 using for loop
+
+public class Q_1 {
+    public static void main(String[] args) {
+        int num = 1;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+// through String
+//        String result = "";
+//
+//        for (int i = 1; i <= 10; i++) {
+//            result += i + " ";
+//        }
+//        System.out.println("Stored numbers: " + result);
+    }
+}
