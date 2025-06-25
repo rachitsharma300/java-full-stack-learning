8ced359f605f27ecda915814c586bde2af867656
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-25 18:04:59 +0530

    [Self Practice] Ques_2 1 to 50 Even only

---
+++ 01-java-core/Self Practice/3 - Loops/Q_2.java
@@ -0,0 +1,12 @@
+// Print even numbers from 1 to 50
+public class Q_2 {
+    public static void main(String[] args) {
+        int num = 50;
+
+        for (int i = 0; i < 50; i++){
+            if (i % 2 == 0) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
