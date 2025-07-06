c9e6e6dabb106e897fcfb957a82a8694c7d8f8b8
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-06 23:47:01 +0530

    Q-3 solved

---
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-3.js
@@ -0,0 +1,10 @@
+// 3. Even or Odd
+// Write a function that checks whether a given number is even or odd.
+
+const num = 10;
+
+if ( num % 2 == 0 ) {
+    console.log("Evne Number");
+} else {
+    console.log('Odd Number');
+}
