0befab74e5aa7fa0fda9c6c5f130981ca2d59689
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-06 23:38:36 +0530

    Q-2 Solved

---
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-2.js
@@ -0,0 +1,8 @@
+// 2. Variables
+// Create two variables num1 and num2, assign them values 10 and 20, and print their sum.
+
+const num1 = 10;
+const num2 = 20;
+
+const sum = num1 + num2;
+console.log(sum);
