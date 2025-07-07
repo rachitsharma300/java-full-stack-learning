878ec7e904646b84e9bbf7b05d73c831f4b10cae
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-07 23:31:17 +0530

    Que-10 added

---
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-10.js
@@ -0,0 +1,2 @@
+// 10. FizzBuzz
+// Write a program that prints numbers from 1 to 20. But for multiples of 3, print “Fizz”, for multiples of 5 print “Buzz”, and for multiples of both print “FizzBuzz”.
