dc3a6e690b54fbb521713b681d223d7fc94b8d16
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-08 14:39:32 +0530

    solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-5.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-5.js
@@ -1,2 +1,18 @@
 // 5. Simple Calculator
 // Create a function calculator that takes three parameters: num1, num2, and operator (+, -, *, /), and returns the result.
+
+function calculator(num1, num2, operator) {
+    if (operator === '+') {
+        return num1 + num2;
+    } else if (operator === '-') {
+        return num1 - num2;
+    } else if (operator === '*') {
+        return num1 * num2;
+    } else if ( operator === '/') {
+        return num1 / num2;
+    } else {
+        return "Invalid operator";
+    }
+}
+
+console.log(calculator(10, 5, '+'));
