287c12c94ec54dd25e9edfc5722d7b61fc70374c
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-08 14:39:39 +0530

    solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-6.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-6.js
@@ -1,2 +1,14 @@
 // 6. Array Sum
 // Given an array [1,2,3,4,5], write a function to calculate and return the sum of all elements.
+
+function arraysum(arr) {
+    let sum = 0;
+    for(let i = 0; i<arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(arraysum(numbers));
