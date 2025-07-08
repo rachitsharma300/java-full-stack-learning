ec3f4575e6ace1f8d8e5bfa5caad4d8fa7cfd4f0
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-08 14:39:47 +0530

    solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-7.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-7.js
@@ -1,2 +1,16 @@
 // 7. Reverse a String
 // Write a function to reverse a given string. Example: “hello” → “olleh”
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+console.log(reverseString("Hello"));
+
+// function reverseString(str) {
+//   let reversed = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
