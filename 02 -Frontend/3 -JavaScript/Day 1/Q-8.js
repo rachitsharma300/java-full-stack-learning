6b15fb1b5558358555932bb38d504e6b1d551320
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-08 14:39:54 +0530

    solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-8.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-8.js
@@ -1,2 +1,12 @@
 // 8. Factorial
 // Write a function to find the factorial of a number using a for loop.
+
+function factorial(n) {
+    let fact = 1;
+    for (let i=1; i<=n; i++) {
+        fact *=i;
+    }
+    return fact;
+}
+
+console.log(factorial(5)); // O/T= 120
