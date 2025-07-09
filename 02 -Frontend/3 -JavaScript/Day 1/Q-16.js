c5b75f2d0154240cc4d5bf2009521ca17d40e4a9
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-09 22:59:39 +0530

    solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-16.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-16.js
@@ -1,2 +1,8 @@
 // 16. Generate Random Number
 // Write a function that generates a random integer between 1 and 100 (inclusive).
+
+function randomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+console.log(randomNumber());
