8c0698794a0ad5866397fc430e3e3ebe3f9bea64
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-09 22:48:45 +0530

    Solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-11.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-11.js
@@ -1,2 +1,13 @@
 // 11. Swap Two Numbers
 // Write a program to swap the values of two variables using a temporary third variable.
+
+let num1 = 10;
+let num2 = 20;
+
+console.log('Original Num: ' + num1 + " " + num2);
+
+let temp = num1; // temp = 10
+num1 = num2;     // num1 = 20
+num2 = temp;     // num2 = 10
+
+console.log('After Swap: ' + num1 + " " + num2);
