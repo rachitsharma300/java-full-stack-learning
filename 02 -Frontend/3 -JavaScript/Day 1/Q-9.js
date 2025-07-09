742d2cbecbee4754139eade559eb9062908cf462
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-09 22:48:29 +0530

    done

--- 02 -Frontend/3 -JavaScript/Day 1/Q-9.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-9.js
@@ -1,2 +1,23 @@
 // 9. Palindrome Check
-// Write a function that checks whether a string is a palindrome or not. (Same forward and backward)
+// Write a function that checks whether a string is a palindrome or not. (Same forward and backward)a
+
+// function isPalindrome(str) {
+//   let reversed = str.split('').reverse().join('');
+//   return str === reversed;
+// }
+
+// console.log(isPalindrome("madam")); // Output: true
+// console.log(isPalindrome("hello")); // Output: false
+
+function isPalindrome(str) {
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+console.log(isPalindrome("madam")); // Output: true
+console.log(isPalindrome("hello")); // Output: false
