5c0b9f26acd783cb50ff9bb190c3164e388abd9c
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-09 23:01:56 +0530

    Solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-18.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-18.js
@@ -1,2 +1,14 @@
 // 18. Sum of Digits
-// Write a function to find the sum of all digits in a given number. Example: 123 → 6.
+// Write a function to find the sum of all digits in a given number. 
+// Example: 123 → 6.
+
+function sumDigits(num) {
+    let sum = 0;
+    let str = num.toString();
+    for (let i = 0; i <str.length; i++) {
+        sum += parseInt(str[i]);
+    }
+    return sum;
+}
+console.log(sumDigits(123)); // OT--6
+console.log(sumDigits(987)); // OT--24
