185e92e792b13472605e95fe5078f3a532e639ea
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-09 22:49:06 +0530

    solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-14.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-14.js
@@ -1,2 +1,9 @@
 // 14. Convert Celsius to Fahrenheit
 // Write a function to convert temperature from Celsius to Fahrenheit. (Formula: F = C * 1.8 + 32)
+
+function celsiusToFahrenheit(celsius) {
+  return celsius * 1.8 + 32;
+}
+
+console.log(celsiusToFahrenheit(0));   // Output: 32
+console.log(celsiusToFahrenheit(25));  // Output: 77
