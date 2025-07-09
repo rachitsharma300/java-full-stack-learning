c117f8e4230142c5fe07bb1a4815b39b7d8cdf60
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-09 23:02:07 +0530

    Solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-20.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-20.js
@@ -1,2 +1,11 @@
 // 20. Merge Two Arrays
 // Write a function to merge two arrays and return a single merged array.
+
+function mergeArrays(arr1, aar2) {
+    return [...arr1, ...aar2];
+}
+
+let a = [1, 2, 3];
+let b = [4, 5, 6];
+
+console.log(mergeArrays(a, b)); // OT-- [1,2,3,4,5,6]
