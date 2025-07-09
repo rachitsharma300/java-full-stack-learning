cb8a6a2b9636178e05537c222369756dd5caf9c7
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-09 22:49:11 +0530

    solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-15.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-15.js
@@ -1,2 +1,16 @@
 // 15. Check for Prime Number
 // Write a function to check whether a given number is prime or not.
+
+function isPrime(n) {
+  if (n <= 1) return false;
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(7));
+console.log(isPrime(10));
