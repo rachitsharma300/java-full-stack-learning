b4b6976d175cd0bff3b55f703979974a1e1e3bd3
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-09 23:01:50 +0530

    Solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-17.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-17.js
@@ -1,2 +1,11 @@
 // 17. Capitalize First Letter
-// Write a function that takes a string and returns it with the first letter capitalized. Example: “hello” → “Hello”.
+// Write a function that takes a string and returns it with the first letter capitalized. 
+// Example: “hello” → “Hello”.
+
+function capitalizeFirst(str) {
+  if (str.length === 0) return ""; // handle empty string
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+console.log(capitalizeFirst("hello")); // Output: "Hello"
+console.log(capitalizeFirst("javaScript")); // Output: "JavaScript"
