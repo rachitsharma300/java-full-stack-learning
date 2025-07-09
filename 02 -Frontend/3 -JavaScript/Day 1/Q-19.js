0ac9646c96a070c565072ce116d395ab225772f8
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-09 23:02:01 +0530

    Solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-19.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-19.js
@@ -1,2 +1,21 @@
 // a19. Remove Duplicates from Array
 // Write a function that removes duplicate elements from an array and returns a new array with unique elements.
+
+function removeDuplicates(arr) {
+    return[...new Set(arr)];
+}
+
+let numbers = [ 1, 2, 3, 4, 5, 5, 6];
+console.log(removeDuplicates(numbers));
+
+// ----- USING LOOPS -----
+
+// function removeDuplicates(arr) {
+//   let unique = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!unique.includes(arr[i])) {
+//       unique.push(arr[i]);
+//     }
+//   }
+//   return unique;
+// }
