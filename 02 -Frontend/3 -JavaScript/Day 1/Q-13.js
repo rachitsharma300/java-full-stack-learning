4ce23ba1cd54e7720275de19aa95bc9283e36f5e
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-09 22:48:58 +0530

    Solved

--- 02 -Frontend/3 -JavaScript/Day 1/Q-13.js
+++ 02 -Frontend/3 -JavaScript/Day 1/Q-13.js
@@ -1,2 +1,15 @@
 // 13. Find Minimum in Array
 // Write a function to find and return the smallest number in an array of numbers.
+
+function findMinimum(arr) {
+  let min = arr[0]; //assume first element in mini..
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+let number = [5, 2, 8, 9, 4, 3];
+console.log(findMinimum(number));
