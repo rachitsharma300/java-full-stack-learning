cf5524773afc3bd75eb79241057cec364f158f3f
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-31 21:58:43 +0530

    feat: DAO layer for mapping CURD OP on MongoDB

--- resources/Assignments/Spring Task/springTask/src/main/java/com/example/springTask/repository/EmployeeRepository.java
+++ resources/Assignments/Spring Task/springTask/src/main/java/com/example/springTask/repository/EmployeeRepository.java
@@ -1,4 +1,11 @@
 package com.example.springTask.repository;
 
-public class EmployeeRepository {
+import com.example.springTask.model.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Repository layer (MongoDB access).
+ * Direct Mapping CURD Operation on MongoDB
+ */
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
 }
