82f556e46954899beff8d4c9fd7b0cb48c39cc95
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-31 21:56:29 +0530

    feat: Employee Entity

--- resources/Assignments/Spring Task/springTask/src/main/java/com/example/springTask/model/Employee.java
+++ resources/Assignments/Spring Task/springTask/src/main/java/com/example/springTask/model/Employee.java
@@ -1,4 +1,34 @@
 package com.example.springTask.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Employee entity (MongoDB collection: employees).
+ * This class represent a document in db.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "employees")
 public class Employee {
+
+    @Id
+    @NotBlank(message = "Employee ID is required")
+    private String employeeId;  // Primary key for MongoDB
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Invalid email")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Location is required")
+    private String location;
 }
