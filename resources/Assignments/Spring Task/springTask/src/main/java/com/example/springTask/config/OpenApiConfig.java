e99630f134263707cf4a542444e9a9af3c48af56
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-31 22:05:30 +0530

    feat: This class configure Swagger UI More Helpful for testing APIs on UI

--- resources/Assignments/Spring Task/springTask/src/main/java/com/example/springTask/config/OpenApiConfig.java
+++ resources/Assignments/Spring Task/springTask/src/main/java/com/example/springTask/config/OpenApiConfig.java
@@ -1,4 +1,24 @@
 package com.example.springTask.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OpenAPI / Swagger configuration.
+ * This class configure Swagger UI
+ * More Helpful for testing APIs on UI
+ */
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI employeeApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Employee API")
+                        .description("REST API for Employee CRUD using Spring Boot + MongoDB")
+                        .version("v1"));
+    }
 }
