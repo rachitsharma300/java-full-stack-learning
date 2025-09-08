c7355f92a0ad88ddfd1b5d3c991a3d48404453ec
rachitsharma300 <rachitsharma300@gmail.com>
2025-09-09 01:50:07 +0530

    feat: implement ProductService with business logic

---
+++ resources/Assignments/Spring Task/Spring Task_2/second_task/src/main/java/com/guvihcl/second_task/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.guvihcl.second_task.service;
+
+
+
+import com.guvihcl.second_task.entity.Product;
+import com.guvihcl.second_task.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategory(category);
+    }
+}
