9d6716ea52a1e6033e3b760191046915a4e50a5f
rachitsharma300 <rachitsharma300@gmail.com>
2025-09-09 01:51:53 +0530

    feat: initialize Spring Boot application with main class

---
+++ resources/Assignments/Spring Task/Spring Task_2/second_task/src/test/java/com/guvihcl/second_task/SecondTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guvihcl.second_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
