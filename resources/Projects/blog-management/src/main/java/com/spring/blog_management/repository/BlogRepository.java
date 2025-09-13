5a94cbd3ee4df6ecf9ca4997db2ae8b9a7b5aa62
rachitsharma300 <rachitsharma300@gmail.com>
2025-09-13 22:36:17 +0530

    feat(repository): Add BlogRepository extending JpaRepository for CRUD operations

--- resources/Projects/blog-management/src/main/java/com/spring/blog_management/repository/BlogRepository.java
+++ resources/Projects/blog-management/src/main/java/com/spring/blog_management/repository/BlogRepository.java
@@ -1,4 +1,11 @@
 package com.spring.blog_management.repository;
 
-public class BlogRepository {
+import com.spring.blog_management.model.BlogPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<BlogPost, Long> {
+
+    // No Additional methods needed for basic CURD Op..
 }
